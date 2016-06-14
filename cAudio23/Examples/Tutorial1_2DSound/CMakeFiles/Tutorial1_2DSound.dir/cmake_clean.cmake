e6433f70043220a63ffab3b53fb7f3d20cd35f80
tecan <teslaenergy@gmail.com>
2016-06-14 09:06:10 -0600

    libtheoraplayer

---
+++ cAudio23/Examples/Tutorial1_2DSound/CMakeFiles/Tutorial1_2DSound.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial1_2DSound.dir/src/main.cpp.o"
+  "Tutorial1_2DSound.pdb"
+  "Tutorial1_2DSound"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial1_2DSound.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cAudio23/Examples/Tutorial6_CustomEventHandler/CMakeFiles/Tutorial6_CustomEventHandler.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial6_CustomEventHandler.dir/src/cTestHandler.cpp.o"
+  "CMakeFiles/Tutorial6_CustomEventHandler.dir/src/main.cpp.o"
+  "Tutorial6_CustomEventHandler.pdb"
+  "Tutorial6_CustomEventHandler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial6_CustomEventHandler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
