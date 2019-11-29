c3cfcdeb050fa2a79892ceb190dde948df49e594
teslaenergy@gmail.com <teslaenergy@gmail.com>
2019-11-29 08:13:42 -0700

    polyvox

---
+++ media/PolyVox/documentation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/documentation
+# Build directory: /run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/documentation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ media/PolyVox/tests/CTestTestfile.cmake
@@ -0,0 +1,20 @@
+# CMake generated Testfile for 
+# Source directory: /run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests
+# Build directory: /run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(AmbientOcclusionGeneratorExecuteTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestAmbientOcclusionGenerator" "testExecute")
+add_test(ArrayReadWriteTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestArray" "testReadWrite")
+add_test(AStarPathfinderExecuteTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestAStarPathfinder" "testExecute")
+add_test(LowPassFilterExecuteTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestLowPassFilter" "testExecute")
+add_test(VolumeSizeTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/testvolume" "testSize")
+add_test(MaterialTestCompile "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/testmaterial" "testCompile")
+add_test(RaycastExecuteTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestRaycast" "testExecute")
+add_test(RegionEqualityTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestRegion" "testEquality")
+add_test(SurfaceExtractorExecuteTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestSurfaceExtractor" "testExecute")
+add_test(VectorLengthTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/testvector" "testLength")
+add_test(VectorDotProductTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/testvector" "testDotProduct")
+add_test(VectorEqualityTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/testvector" "testEquality")
+add_test(VolumeSubclassExtractSurfaceTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestVolumeSubclass" "testExtractSurface")
+add_test(VoxelsTraitsTest "/run/media/netpipe/df7f53ef-2a98-4562-a498-7da578dab660/games/Luna/LunaLibs-current/media/PolyVox/tests/TestVoxels" "testVoxelTypeLimits")
